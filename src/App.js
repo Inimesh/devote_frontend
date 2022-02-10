cfba3fa5b63e6e780018a5b01ef3567ce9a1d1ec
Sarah <dousse.sarah@gmail.com>
2022-02-10 16:37:33 +0000

    TODO: fix routing Home , Login, Signup components

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import axios from "axios";
 import Configs from "./components/configs.js";
 import { useEffect, useState } from "react";
 import React, { Component } from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./components/Home.js";
+import Login from "./components/Login.js";
+import Signup from "./components/Signup.js";
 
 const API_URL = "http://localhost:3000/api/configs";
 
@@ -11,72 +14,74 @@ function getAPIData() {
   return axios.get(API_URL).then((response) => response.data);
 }
 
-function App() {
-  const [configs, setConfigs] = useState([]);
+// function App() {
+//   const [configs, setConfigs] = useState([]);
 
-  useEffect(() => {
-    let mounted = true;
-    getAPIData().then((items) => {
-      if (mounted) {
-        setConfigs(items);
-      }
-    });
-    return () => (mounted = false);
-  }, []);
+//   useEffect(() => {
+//     let mounted = true;
+//     getAPIData().then((items) => {
+//       if (mounted) {
+//         setConfigs(items);
+//       }
+//     });
+//     return () => (mounted = false);
+//   }, []);
 
-  return (
-    <div className="App">
-      <h1>Hello</h1>
-      <Configs configs={configs} />
-    </div>
-  );
-}
+//   return (
+//     <div className="App">
+//       <h1>Hello</h1>
+//       <Configs configs={configs} />
+//     </div>
+//   );
+// }
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { 
+    this.state = {
       isLoggedIn: false,
-      user: {}
-     };
+      user: {},
+    };
   }
-componentDidMount() {
-  this.loginStatus()
-}
-loginStatus = () => {
-    axios.get('http://localhost:3001/logged_in', 
-    {withCredentials: true})    
-.then(response => {
-      if (response.data.logged_in) {
-        this.handleLogin(response)
-      } else {
-        this.handleLogout()
-      }
-    })
-    .catch(error => console.log('api errors:', error))
+  componentDidMount() {
+    this.loginStatus();
   }
-handleLogin = (data) => {
+  loginStatus = () => {
+    axios
+      .get("http://localhost:3000/api/logged_in", { withCredentials: true })
+      .then((response) => {
+        if (response.data.logged_in) {
+          this.handleLogin(response);
+        } else {
+          this.handleLogout();
+        }
+      })
+      .catch((error) => console.log("api errors:", error));
+  };
+  handleLogin = (data) => {
     this.setState({
       isLoggedIn: true,
-      user: data.user
-    })
-  }
-handleLogout = () => {
+      user: data.user,
+    });
+  };
+  handleLogout = () => {
     this.setState({
-    isLoggedIn: false,
-    user: {}
-    })
-  }
-  
+      isLoggedIn: false,
+      user: {},
+    });
+  };
+
   render() {
     return (
       <div>
-         <BrowserRouter>
-          <Switch>
-            <Route exact path='/' component={}/>
-            <Route exact path='/login' component={}/>
-            <Route exact path='/signup' component={}/>
-          </Switch>
+        <BrowserRouter>
+          <Routes>
+            <Route exact path="/" element={<Home />}>
+              <Home/>
+            </Route>
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/signup" component={Signup} />
+          </Routes>
         </BrowserRouter>
       </div>
     );
