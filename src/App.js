b79b91c1ce1962ab3c146bbdca620dcd65cf8e80
Christopher <dagswebb@gmail.com>
2022-02-11 16:23:21 +0000

    created static page with dummy JSONs and two containers

--- src/App.js
+++ src/App.js
@@ -1,34 +1,94 @@
-import './App.css';
-import axios from 'axios';
-import Configs from "./components/configs.js";
-import {useEffect, useState } from "react";
-
-const API_URL = "http://localhost:3000/api/configs";
-
-function getAPIData() {
-  return axios.get(API_URL).then((response => response.data))
-}
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 function App() {
+  class Dashboard extends React.Component {
+    render() {
+      return(<div>Welcome, Username!</div>)
+    }
+  }
 
-  const [configs, setConfigs] = useState([]);
+  class SearchBar extends React.Component {
+    render() {
+      return (
+        <form>
+          <input type="text" placeholder="Search..." />
+          <p>
+            <input type="checkbox" />
+            {' '}
+            Only show products in stock
+          </p>
+        </form>
+      );
+    }
+  }
 
-  useEffect(() => {
-    let mounted = true;
-    getAPIData().then((items) => {
-      if (mounted) {
-        setConfigs(items);
-      }
-    });
-    return () => (mounted = false);
-  }, []);
+  class User extends React.Component {
+    // constructor(props) {
+    //   super(props);
+    // }
+    render() {
+      return (
+      <div>
+        <h1>Welcome, User!</h1>
+        <UserJSON details={["john doe", "johndoe@email.com"]}/>
+        <TransactionList/>
+      </div>
+      );
+    }
+  }
 
-  return (
-    <div className="App">
-      <h1>Hello</h1>
-      <Configs configs={configs} />
-    </div>
-  );
+  class TransactionList extends React.Component {
+    // constructor(props) {
+    //   super(props);
+    // }
+    render() {
+      return (
+      <div>
+        <h1>Your Transactions</h1>
+        <TransactionsListJSON transactions={["Starbucks: 3.98", "HMV: 44.99", "TFL: 5.60"]}/>
+      </div>
+      );
+    }
+  }
+
+  class ReceiverAccount extends React.Component {
+    // constructor(props) {
+    //   super(props);
+    // }
+    render() {
+      return (
+      <div>
+        <h1>Your Pots</h1>
+        <ReceiverAccountJSON receiver={["Savings Pot", "British Heart Foundation", "Cancer Research"]}/>
+      </div>
+      );
+    }
+  }
+
+  const ReceiverAccountJSON = props => {
+    return <p>{props.receiver.join(", ")}</p>
+  };
+
+  const TransactionsListJSON = props => {
+    return <p>{props.transactions.join(", ")}</p>
+  };
+  
+  const UserJSON = props => {
+    return <p> {props.details.join(", ")}</p>
+};
+
+
+ReactDOM.render(
+  <User> <TransactionList/> </User>, 
+  document.getElementById('main-container')
+);
+
+ReactDOM.render(
+  <ReceiverAccount/>,
+  document.getElementById('second-container')
+);
 }
+
 
 export default App;
