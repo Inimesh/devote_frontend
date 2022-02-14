de68abfeb38ee092a582860a9b42f4fa8277e809
Inimesh <ijmeshoulam@gmail.com>
2022-02-14 14:37:54 +0000

    Adding text placeholder for avatar picture

--- src/components/dashboard/UserInfoDisplay.js
+++ src/components/dashboard/UserInfoDisplay.js
@@ -3,7 +3,7 @@ import React from 'react'
 const UserInfoDisplay = (props) => {
   return (
     <div>
-      {/* Avatar picture */}
+      <p>*Avatar picture here*</p>
       <h2>{props.username}</h2>
       <h3>{props.email}</h3>
     </div>
