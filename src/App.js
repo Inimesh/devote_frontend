a8e6288fc36ec448cc50ed5b8dbdf219882be565
Christopher <dagswebb@gmail.com>
2022-02-14 12:34:46 +0000

    created session storage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
 import Preferences from './components/Preferences';
 import Login from './components/Login';
 
+function setToken(userToken){
+  sessionStorage.setItem('token', JSON.stringify(userToken));
+}
+
+function getToken() {
+  const tokenString = sessionStorage.getItem('token');
+  const userToken = JSON.parse(tokenString);
+  return userToken?.token
+}
+
 function App() {
-  const [token, setToken] = useState();
+  const token = getToken();
   if(!token) {
     return <Login setToken={setToken} />
   }
