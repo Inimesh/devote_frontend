24fd698b2e06cabe152f88293358e8ee0bb2b395
Sarah <dousse.sarah@gmail.com>
2022-02-14 14:39:57 +0000

    create custom hook

--- src/App.js
+++ src/App.js
@@ -4,28 +4,31 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
 import Preferences from './components/Preferences';
 import Login from './components/Login';
+import useToken from './components/useToken';
 
-function setToken(userToken){
-  sessionStorage.setItem('token', JSON.stringify(userToken));
-}
+// function setToken(userToken){
+//   sessionStorage.setItem('token', JSON.stringify(userToken));
+// }
 
-function getToken() {
-  const tokenString = sessionStorage.getItem('token');
-  const userToken = JSON.parse(tokenString);
-  return userToken?.token
-}
+// function getToken() {
+//   const tokenString = sessionStorage.getItem('token');
+//   const userToken = JSON.parse(tokenString);
+//   return userToken?.token
+// }
 
 function App() {
-  const token = getToken();
+  const { token, setToken } = useToken();
+
   if(!token) {
     return <Login setToken={setToken} />
   }
+
   return (
     <div className="wrapper">
       <h1>Application</h1>
       <Router>
         <Routes>
-          <Route path="/Dashboard" element={<Dashboard/>} />
+          <Route path="/" element={<Dashboard/>} />
           <Route path="/preferences" element={<Preferences/>} />
         </Routes>
       </Router>
