e5a76240df2bd5094ef63532e26e5b51d3db182f
Francesco <francescocollotto@gmail.com>
2022-02-14 15:44:27 +0000

    add styling to transactions table

--- src/components/dashboard/Transaction.js
+++ src/components/dashboard/Transaction.js
@@ -6,8 +6,8 @@ const Transaction = (props) => {
   const emptySpan = <span className="ps-5">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>;
   return (
     <p className="col-12 d-flex justify-content-between">
-      <span className="fw-bold">{props.name}<br /> {formatDate(props.created_at)}</span>
-      <span><span>{props.amount}</span> {roundAmount ? <span className="ps-5">+{roundAmount}</span> : emptySpan}</span>
+      <span><span className="fw-bold">{props.name}</span><br /> <span>{formatDate(props.created_at)}</span></span>
+      <span><span className="fw-bold">-{props.amount}</span> {roundAmount ? <span className="ps-5 fw-bold round-up-amount">+{roundAmount}</span> : emptySpan}</span>
     </p>
   )
 }
