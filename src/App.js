03b8b81a73054910007288b24bf0548b113c03cb
Christopher <dagswebb@gmail.com>
2022-02-14 11:13:40 +0000

    set up router for application, dashboard, preferences

--- src/App.js
+++ src/App.js
@@ -1,94 +1,142 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Dashboard from './Dashboard';
+import Preferences from './Preferences';
 
 function App() {
-  class Dashboard extends React.Component {
-    render() {
-      return(<div>Welcome, Username!</div>)
-    }
-  }
-
-  class SearchBar extends React.Component {
-    render() {
-      return (
-        <form>
-          <input type="text" placeholder="Search..." />
-          <p>
-            <input type="checkbox" />
-            {' '}
-            Only show products in stock
-          </p>
-        </form>
-      );
-    }
-  }
-
-  class User extends React.Component {
-    // constructor(props) {
-    //   super(props);
-    // }
-    render() {
-      return (
-      <div>
-        <h1>Welcome, User!</h1>
-        <UserJSON details={["john doe", "johndoe@email.com"]}/>
-        <TransactionList/>
-      </div>
-      );
-    }
-  }
-
-  class TransactionList extends React.Component {
-    // constructor(props) {
-    //   super(props);
-    // }
-    render() {
-      return (
-      <div>
-        <h1>Your Transactions</h1>
-        <TransactionsListJSON transactions={["Starbucks: 3.98", "HMV: 44.99", "TFL: 5.60"]}/>
-      </div>
-      );
-    }
-  }
-
-  class ReceiverAccount extends React.Component {
-    // constructor(props) {
-    //   super(props);
-    // }
-    render() {
-      return (
-      <div>
-        <h1>Your Pots</h1>
-        <ReceiverAccountJSON receiver={["Savings Pot", "British Heart Foundation", "Cancer Research"]}/>
-      </div>
-      );
-    }
-  }
-
-  const ReceiverAccountJSON = props => {
-    return <p>{props.receiver.join(", ")}</p>
-  };
-
-  const TransactionsListJSON = props => {
-    return <p>{props.transactions.join(", ")}</p>
-  };
-  
-  const UserJSON = props => {
-    return <p> {props.details.join(", ")}</p>
-};
-
-
-ReactDOM.render(
-  <User> <TransactionList/> </User>, 
-  document.getElementById('main-container')
-);
-
-ReactDOM.render(
-  <ReceiverAccount/>,
-  document.getElementById('second-container')
-);
+  return (
+    <div className="wrapper">
+      <h1>Application</h1>
+      <Router>
+        <Routes>
+          <Route path="/Dashboard" element={<Dashboard/>} />
+          <Route path="/preferences" element={<Preferences/>} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
-
 export default App;
+
+
+
+
+
+
+// import React from 'react';
+// import './App.css';
+// import ReactDOM from 'react-dom';
+// import Dashboard from './Dashboard';
+// import Preferences from './Preferences';
+// import { BrowserRouter, Route, Routes } from 'react-router-dom'; 
+
+
+// function App() {
+ 
+//   return (
+//   <div className="wrapper">
+//       <h1>Application</h1>
+//       <BrowserRouter>
+//         <Routes>
+//           <Route path="/Dashboard" element={<Dashboard/>} />
+//             <Dashboard />
+//           {/* </Route> */}
+//           <Route path="/Preferences" element={<Preferences/>} />
+//             <Preferences />
+//           {/* </Route> */}
+//         </Routes>
+//       </BrowserRouter>
+//     </div>
+//   );
+
+// }
+
+//   class SearchBar extends React.Component {
+//     render() {
+//       return (
+//         <form>
+//           <input type="text" placeholder="Search..." />
+//           <button> Logout </button>
+//         </form>
+//       );
+//     }
+//   }
+
+//   class User extends React.Component {
+//     // constructor(props) {
+//     //   super(props);
+//     // }
+//     render() {
+//       return (
+//       <div>
+//         <h1>Welcome, User!</h1>
+//         <UserJSON details={["john doe", "johndoe@email.com"]}/>
+//         <TransactionList/>
+//       </div>
+//       );
+//     }
+//   }
+
+//   class TransactionList extends React.Component {
+//     // constructor(props) {
+//     //   super(props);
+//     // }
+//     render() {
+//       return (
+//       <div>
+//         <h2>Your Recent Transactions</h2>
+//         <TransactionsListJSON transactions={["Starbucks: 3.98", "HMV: 44.99", "TFL: 5.60"]}/>
+//       </div>
+//       );
+//     }
+//   }
+
+//   class ReceiverAccount extends React.Component {
+//     // constructor(props) {
+//     //   super(props);
+//     // }
+//     render() {
+//       return (
+//       <div>
+//         <h2>Your Pots</h2>
+//         <ReceiverAccountJSON receiver={["Savings Pot", "British Heart Foundation", "Cancer Research"]}/>
+//       </div>
+//       );
+//     }
+//   }
+
+//   const ReceiverAccountJSON = props => {
+//     return <p>{props.receiver.join(", ")}</p>
+//   };
+
+//   const TransactionsListJSON = props => {
+//     return <p>{props.transactions.join(", ")}</p>
+//   };
+  
+//   const UserJSON = props => {
+//     return <p> {props.details.join(", ")}</p>
+// };
+
+// ReactDOM.render(
+//   <SearchBar/>,
+//   document.getElementById('search-bar')
+// );
+
+// ReactDOM.render(
+//   <User> <TransactionList/> </User>, 
+//   document.getElementById('main-container')
+// );
+
+// ReactDOM.render(
+//   <ReceiverAccount/>,
+//   document.getElementById('second-container')
+// );
+
+// ReactDOM.render(
+//   <App/>,
+//   document.getElementById('main-container')
+// );
+
+// export default App;
