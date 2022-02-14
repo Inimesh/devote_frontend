793004c3e6f7e3608bacc96656e6e14bb5cfed35
Christopher <dagswebb@gmail.com>
2022-02-14 11:48:36 +0000

    Final commit for pre-node and express login

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Dashboard from './Dashboard';
-import Preferences from './Preferences';
+import Dashboard from './components/Dashboard';
+import Preferences from './components/Preferences';
+import Login from './components/Login';
 
 function App() {
+  const [token, setToken] = useState();
+  if(!token) {
+    return <Login setToken={setToken} />
+  }
   return (
     <div className="wrapper">
       <h1>Application</h1>
