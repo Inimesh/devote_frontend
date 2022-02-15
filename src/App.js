23233dd7a9230aa3bf708ddefef9b6050027a56d
Francesco <francescocollotto@gmail.com>
2022-02-15 16:23:37 +0000

    add basic log in

--- src/App.js
+++ src/App.js
@@ -33,55 +33,118 @@ import Dashboard from "./components/dashboard/Dashboard";
 // }
 
 // API calling ----------------------------------------------------------
-const user = {
-  id: 1
-}
-const API_URL = `http://localhost:3000/api/transactions?user_id=${user.id}`;
-const USER_INFO_API_URL = `http://localhost:3000/api/users/${user.id}`;
+// const user = {
+//   id: 1
+// }
+// const TRANSACTIONS_API_URL = `http://localhost:3000/api/transactions?user_id=${user.id}`;
+// const USER_INFO_API_URL = `http://localhost:3000/api/users/${user.id}`;
 
-function getAPIData() {
-  return axios.get(API_URL).then((response => response.data))
+function getTransactionsAPIData(userId) {
+  return axios.get(`http://localhost:3000/api/transactions?user_id=${userId}`).then((response => response.data))
 }
 
-function getUserInfoAPIData() {
-  return axios.get(USER_INFO_API_URL).then((response => response.data))
-}
+// function getUserInfoAPIData() {
+//   return axios.get(USER_INFO_API_URL).then((response => response.data))
+// }
 
 function App() {
 
   const [transactions, setTransactions] = useState([]);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [userInfo, setUserInfo] = useState();
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    const user = {
+      user: { username: username, password: password }
+    };
+    // send the username and password to the server
+    const response = await axios.post(
+      'http://localhost:3000/api/login',
+      user
+    );
+    // set the state of the user
+    setUserInfo(response.data)
+    // store the user in localStorage
+    localStorage.setItem('user', JSON.stringify(response.data))
+    console.log(response.data)
+    getTransactionsAPIData(response.data.user.id).then((transactions) => {
+      console.log(transactions)
+      console.log("in UseEffect 3")
+      setTransactions(transactions);
+  });
+  };
 
   useEffect(() => {
-    let mounted = true;
-    getAPIData().then((transactions) => {
-      if (mounted) {
-        console.log(transactions)
-        setTransactions(transactions);
-      }
-    });
-    return () => (mounted = false);
+    console.log("in UseEffect 1")
+    const loggedInUser = localStorage.getItem("user");
+    if (loggedInUser) {
+      console.log("in UseEffect 2")
+      const foundUser = JSON.parse(loggedInUser);
+      setUserInfo(foundUser);
+      getTransactionsAPIData(foundUser.user.id).then((transactions) => {
+          console.log(transactions)
+          console.log("in UseEffect 3")
+          setTransactions(transactions);
+      });
+    }
   }, []);
 
+  // useEffect(() => {
+  //   let mounted = true;
+  //   getTransactionsAPIData(userInfo.user.id).then((transactions) => {
+  //     if (mounted) {
+  //       console.log(transactions)
+  //       setTransactions(transactions);
+  //     }
+  //   });
+  //   return () => (mounted = false);
+  // }, []);
 
-  const [userInfo, setUserInfo] = useState([]);
 
-  useEffect(() => {
-    let mounted = true;
-    getUserInfoAPIData().then((userInfo) => {
-      if (mounted) {
-        setUserInfo(userInfo);
-      }
-    });
-    return () => (mounted = false);
-  }, []);
+  // useEffect(() => {
+  //   let mounted = true;
+  //   getUserInfoAPIData().then((userInfo) => {
+  //     if (mounted) {
+  //       setUserInfo(userInfo);
+  //     }
+  //   });
+  //   return () => (mounted = false);
+  // }, []);
 
 
   // Rendering to screen ---------------------------------------------------
+  if (userInfo) {
+    return (
+      <div className="App">
+        <Dashboard transactions={transactions} userInfo={userInfo.user}/>
+      </div>
+    );
+  }
+
   return (
-    <div className="App">
-      <Dashboard transactions={transactions} userInfo={userInfo}/>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="username">Username: </label>
+      <input
+        type="text"
+        value={username}
+        placeholder="enter a username"
+        onChange={({ target }) => setUsername(target.value)}
+      />
+      <div>
+        <label htmlFor="password">password: </label>
+        <input
+          type="password"
+          value={password}
+          placeholder="enter a password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">Login</button>
+    </form>
   );
+
 }
 
 export default App;
