6ed96ff07b46595aa0b41664c43062ecfdd9b2c7
Inimesh <ijmeshoulam@gmail.com>
2022-02-15 20:06:54 +0000

    Grouping transactions by date

--- src/components/dashboard/Transaction.js
+++ src/components/dashboard/Transaction.js
@@ -1,12 +1,11 @@
 import React from "react";
-import {formatDate} from "../../helperFunctions";
 
 const Transaction = (props) => {
   const roundAmount = props.round_up;
   const emptySpan = <span className="ps-5">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>;
   return (
     <p className="col-12 d-flex justify-content-between">
-      <span><span className="fw-bold">{props.name}</span><br /> <span>{formatDate(props.created_at)}</span></span>
+      <span><span className="fw-bold">{props.name}</span><br /> </span>
       <span><span className="fw-bold">£{props.amount}</span> {roundAmount ? <span className="ps-5 fw-bold round-up-amount">+£{roundAmount}</span> : emptySpan}</span>
     </p>
   )
