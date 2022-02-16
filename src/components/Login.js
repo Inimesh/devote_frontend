e266432618617e22cb68a1202edb0e47019047b2
Sarah <dousse.sarah@gmail.com>
2022-02-16 16:11:55 +0000

    basic styling

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,26 +1,40 @@
 import React from 'react';
+import "./Login.css"
 
 const Login = ({handleSubmit, username, setUsername, password, setPassword}) => {
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="username">Username: </label>
-      <input
-        type="text"
-        value={username}
-        placeholder="enter a username"
-        onChange={({ target }) => setUsername(target.value)}
-      />
-      <div>
-        <label htmlFor="password">password: </label>
-        <input
-          type="password"
-          value={password}
-          placeholder="enter a password"
-          onChange={({ target }) => setPassword(target.value)}
-        />
-      </div>
-      <button type="submit">Login</button>
-    </form>
+    <div className="form-signin">
+      <form onSubmit={handleSubmit}>
+        <img className="mb-4 text-center" />
+        <h1 className="h3 mb-3 fw-normal text-center">Please login</h1>
+        <div className="form-floating text-center">
+          <label htmlFor="username">Username: </label>
+          <input
+            className="form-control"
+            type="text"
+            value={username}
+            placeholder="Username"
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div className="form-floating text-center">
+          <label htmlFor="password">password: </label>
+          <input
+            className="form-control"
+            type="password"
+            value={password}
+            placeholder="Password"
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button
+          className="w-100 Login-btn btn btn-lg btn-primary"
+          type="submit"
+        >
+          Login
+        </button>
+      </form>
+    </div>
   );
 }
 
