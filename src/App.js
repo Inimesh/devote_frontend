8b37b930ffc3827c16b46c75f3efd3103608cbe8
Francesco <francescocollotto@gmail.com>
2022-02-16 14:34:39 +0000

    add receiverAccountInfoApi to work with authentication

--- src/App.js
+++ src/App.js
@@ -1,53 +1,18 @@
 import './App.css';
 import axios from 'axios';
-import Configs from "./components/configs.js";
 import {useEffect, useState } from "react";
 import Dashboard from "./components/dashboard/Dashboard";
 import Login from "./components/Login"
 import SignUp from './components/SignUp';
 
-// const API_URL = "http://localhost:3000/api/configs";
-
-// function getAPIData() {
-//   return axios.get(API_URL).then((response => response.data))
-// }
-
-// function App() {
-
-//   const [configs, setConfigs] = useState([]);
-
-//   useEffect(() => {
-//     let mounted = true;
-//     getAPIData().then((items) => {
-//       if (mounted) {
-//         setConfigs(items);
-//       }
-//     });
-//     return () => (mounted = false);
-//   }, []);
-
-//   return (
-//     <div className="App">
-//       <h1>Hello</h1>
-//       <Configs configs={configs} />
-//     </div>
-//   );
-// }
-
-// API calling ----------------------------------------------------------
-// const user = {
-//   id: 1
-// }
-// const TRANSACTIONS_API_URL = `http://localhost:3000/api/transactions?user_id=${user.id}`;
-// const USER_INFO_API_URL = `http://localhost:3000/api/users/${user.id}`;
 
 function getTransactionsAPIData(userId) {
   return axios.get(`http://localhost:3000/api/transactions?user_id=${userId}`).then((response => response.data))
 }
 
-// function getUserInfoAPIData() {
-//   return axios.get(USER_INFO_API_URL).then((response => response.data))
-// }
+function getAllReceiverAccountAPIData() {
+  return axios.get(`http://localhost:3000/api/receiver_accounts`).then((response => response.data))
+}
 
 function App() {
 
@@ -61,6 +26,8 @@ function App() {
   const [email, setEmail] = useState("");
   const [passwordConfirmation, setPasswordConfirmation] = useState("");
   const [percentage, setPercentage] = useState("");
+
+  const [receiverAccountInfo, setReceiverAccountInfo] = useState([]);
 
   const handleLogin = async e => {
     e.preventDefault();
@@ -76,8 +43,17 @@ function App() {
     setUserInfo(response.data)
     // store the user in localStorage
     localStorage.setItem('user', JSON.stringify(response.data))
+    
     getTransactionsAPIData(response.data.user.id).then((transactions) => {
       setTransactions(transactions);
+    });
+
+    getAllReceiverAccountAPIData().then((allReceiverAccountInfo) => {
+      const userReceiverAccountInfo = allReceiverAccountInfo.filter(account => {
+          return account.user_id === response.data.user.id
+        });
+        setReceiverAccountInfo(userReceiverAccountInfo);
+        console.log(receiverAccountInfo)
     });
   };
 
@@ -131,8 +107,16 @@ function App() {
 
     setUserInfo(response.data)
     localStorage.setItem('user', JSON.stringify(response.data))
+    
     getTransactionsAPIData(response.data.user.id).then((transactions) => {
       setTransactions(transactions);
+    });
+
+    getAllReceiverAccountAPIData().then((allReceiverAccountInfo) => {
+      const userReceiverAccountInfo = allReceiverAccountInfo.filter(account => {
+          return account.user_id === response.data.user.id
+        });
+        setReceiverAccountInfo(userReceiverAccountInfo);
     });
   };
 
@@ -151,30 +135,15 @@ function App() {
       getTransactionsAPIData(foundUser.user.id).then((transactions) => {
           setTransactions(transactions);
       });
+
+      getAllReceiverAccountAPIData().then((allReceiverAccountInfo) => {
+        const userReceiverAccountInfo = allReceiverAccountInfo.filter(account => {
+            return account.user_id === foundUser.user.id
+          });
+          setReceiverAccountInfo(userReceiverAccountInfo);
+      });
     }
   }, []);
-
-  // useEffect(() => {
-  //   let mounted = true;
-  //   getTransactionsAPIData(userInfo.user.id).then((transactions) => {
-  //     if (mounted) {
-  //       console.log(transactions)
-  //       setTransactions(transactions);
-  //     }
-  //   });
-  //   return () => (mounted = false);
-  // }, []);
-
-
-  // useEffect(() => {
-  //   let mounted = true;
-  //   getUserInfoAPIData().then((userInfo) => {
-  //     if (mounted) {
-  //       setUserInfo(userInfo);
-  //     }
-  //   });
-  //   return () => (mounted = false);
-  // }, []);
 
 
   // Rendering to screen ---------------------------------------------------
@@ -192,27 +161,7 @@ function App() {
         <SignUp handleSubmit={handleSignUp} username={signupUsername} setUsername={setSignupUsername} email={email} setEmail={setEmail} password={signupPassword} setPassword={setSignupPassword} passwordConfirmation={passwordConfirmation} setPasswordConfirmation={setPasswordConfirmation} setPercentage={setPercentage} />
       </div>
     )
-  // return (
-  //   <form onSubmit={handleSubmit}>
-  //     <label htmlFor="username">Username: </label>
-  //     <input
-  //       type="text"
-  //       value={username}
-  //       placeholder="enter a username"
-  //       onChange={({ target }) => setUsername(target.value)}
-  //     />
-  //     <div>
-  //       <label htmlFor="password">password: </label>
-  //       <input
-  //         type="password"
-  //         value={password}
-  //         placeholder="enter a password"
-  //         onChange={({ target }) => setPassword(target.value)}
-  //       />
-  //     </div>
-  //     <button type="submit">Login</button>
-  //   </form>
-  // );
+
 
 }
 
--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -9,7 +9,7 @@ const Dashboard = (props) => {
       <div className="row">
         <div className="col-sm-2"><UserWindow className="user_window" handleLogout={props.handleLogout} userInfo={props.userInfo} transactions={props.transactions}/></div>
         <div className="col-sm-6"><ListTransactions transactions={props.transactions} /></div>
-        <div className="col-sm-4">Pots{/* Pots components */}</div>
+        <div className="col-sm-4"></div>
       </div>
     </div>
   )
