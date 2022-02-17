e50fc3b89040dfa6493035f8f359b7142418959f
Sarah <dousse.sarah@gmail.com>
2022-02-17 11:00:48 +0000

    2 columns css styling for login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,41 +1,61 @@
-import React from 'react';
-import "./Login.css"
+import React from "react";
+import "./Login.css";
+import Logo from "./devotelogo.png";
 
-const Login = ({handleSubmit, username, setUsername, password, setPassword}) => {
+const Login = ({
+  handleSubmit,
+  username,
+  setUsername,
+  password,
+  setPassword,
+}) => {
   return (
-    <div className="form-login">
-      <form onSubmit={handleSubmit}>
-        <img src="../../public/favicon.ico" className="mb-4 text-center" />
-        <h1 className="h3 mb-3 fw-normal text-center">Please login</h1>
-        <div className="form-floating text-center">
-          <label htmlFor="username">Username: </label>
-          <input
-            className="form-control"
-            type="text"
-            value={username}
-            placeholder="Username"
-            onChange={({ target }) => setUsername(target.value)}
+    <div className="loginContainer">
+      <div className="row align-items-center justify-content-center vh-100">
+        <div className="col-md-5 align-content-center">
+          <img
+            className="rounded mx-auto d-block w-75 p-3"
+            src={Logo}
+            alt="Devote logo"
           />
+          <h4 className="text-center">
+            Saving a <em>small</em> amount soon builds up to a large amount.
+          </h4>
         </div>
-        <div className="form-floating text-center">
-          <label htmlFor="password">password: </label>
-          <input
-            className="form-control"
-            type="password"
-            value={password}
-            placeholder="Password"
-            onChange={({ target }) => setPassword(target.value)}
-          />
+        <div className="col-md-5">
+          <form onSubmit={handleSubmit}>
+            <h3>Please login</h3>
+            <div className="mb-3">
+              {/* <label htmlFor="username">Username: </label> */}
+              <input
+                className="form-control"
+                type="text"
+                value={username}
+                placeholder="Username"
+                onChange={({ target }) => setUsername(target.value)}
+              />
+            </div>
+            <div className="mb-3">
+              {/* <label htmlFor="password">password: </label> */}
+              <input
+                className="form-control"
+                type="password"
+                value={password}
+                placeholder="Password"
+                onChange={({ target }) => setPassword(target.value)}
+              />
+            </div>
+            <button
+              className="w-100 login-btn btn btn-lg btn-primary"
+              type="submit"
+            >
+              Login
+            </button>
+          </form>
         </div>
-        <button
-          className="w-100 login-btn btn btn-lg btn-primary"
-          type="submit"
-        >
-          Login
-        </button>
-      </form>
+      </div>
     </div>
   );
-}
+};
 
 export default Login;
