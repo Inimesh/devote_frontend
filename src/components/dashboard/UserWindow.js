5ba77b286326b4ca8df4e2a7aaf9778f6a366a40
Inimesh <ijmeshoulam@gmail.com>
2022-02-17 10:39:51 +0000

    Removing unused TotalRounded component from UserWindow

--- src/components/dashboard/UserWindow.js
+++ src/components/dashboard/UserWindow.js
@@ -10,7 +10,6 @@ const UserWindow = (props) => {
       {/* UserInfoMenu */}
       <UserInfoDisplay username={props.userInfo.username} email={props.userInfo.email} />
       <button type="button" className="btn btn-dark btn-sm mb-4" onClick={props.handleLogout}>logout</button>
-      <TotalRounded transactions={props.transactions} receiverAccountInfo={props.receiverAccountInfo}/>
     </div>
   )
 }
