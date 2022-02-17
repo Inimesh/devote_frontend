442ea0d7b0c4f9b42410d69f02335911099add12
Inimesh <ijmeshoulam@gmail.com>
2022-02-17 10:51:14 +0000

    Adding function to randomize avatar appearence

--- src/components/dashboard/UserWindow.js
+++ src/components/dashboard/UserWindow.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import UserInfoDisplay from './UserInfoDisplay';
 import Avatar from "avataaars"; // There are apparently vulnerabilities in avataaars dependencies
+import { generateRandomAvatarOptions } from "./avatar.js"
 import UserInfoMenu from './Navbar';
 
 const UserWindow = (props) => {
@@ -11,7 +12,8 @@ const UserWindow = (props) => {
       {/* UserInfoMenu */}
       <Avatar
         style={{ width: '100px', height: '100px' }}
-        avatarStyle="Circle" />
+        avatarStyle="Circle"
+        {...generateRandomAvatarOptions() } />
       <UserInfoDisplay username={props.userInfo.username} email={props.userInfo.email} />
       <button type="button" className="btn btn-dark btn-sm mb-4" onClick={props.handleLogout}>logout</button>
     </div>
